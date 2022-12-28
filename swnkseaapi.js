687f4cbae1001a24d00ca20e6f8e5f589ff991e0
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2022-12-28 19:31:00 -0300

    Add files via upload

---
+++ swnkseaapi.js
@@ -0,0 +1,12 @@
+//LICENSE: Licensed with CC-NC-ND
+//Author: SwankyNoob
+//Version: 1.0.1
+
+//Get the user's email and username
+
+    alert("This app requires a SwankySea login. After clicking OK, you will be asked for your Email and Username. No SwankySea app, other than SwankySea itself, will ask you for your password, be careful! Remember that if the data sent by you do not match an existing account, one will be automatically created.")
+    var email = prompt("Enter your Email (From a SwankySea account)")
+    var username = prompt("Enter your Username (From a SwankySea account)")
+    alert("You have been logged in as " + username)
+
+
